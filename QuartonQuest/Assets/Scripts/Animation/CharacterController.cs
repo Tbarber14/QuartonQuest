086368eff8c28cd428286e27307d755ea118381b
mccamyir <49794426+mccamyir@users.noreply.github.com>
2020-04-13 20:08:50 -0500

    Add additional animations (#102)
    * Update OnDeckTIle.mat
    
    * Add Character Idle Animation
    
    Revised Story part 2 to match new ship design
    
    * Add Character Idle Animation
    
    Revised Story part 2 to match new ship design
    
    * Set Default Character Material
    
    Defaults to the main character's skin material. The other material can be applied when the prefab model is set into the scene, the dragging and dropping the new material onto the model portion of the prefab.
    
    * Set Default Character Material
    
    Defaults to the main character's skin material. The other material can be applied when the prefab model is set into the scene, the dragging and dropping the new material onto the model portion of the prefab.
    
    * Update OnDeckTIle.mat
    
    * Update OnDeckTIle.mat
    
    * Add additional Animations
    
    * Add additional Animations
    
    * Add additional Animations
    
    * Add Animation Events
    
    Player character needs to be named "PlayerCharacter" for script to work.
    
    Opponent character will occasionally play the thinking animation (even though it's not supposed to), but since it transitions better than the player character's version, that seems to be ok.
    
    * Fix Animation Positioning
    
    * Fix Animation Errors
    
    * Minor fixes
    
    * Update Story_Comic_part2.png

---
+++ QuartonQuest/Assets/Scripts/Animation/CharacterController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Animations;
+
+public class CharacterController : MonoBehaviour
+{
+    public Animator charAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        charAnim = GetComponent<Animator>();
+        GameCoreController.Instance.GameOver += GameOver;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameCoreController.Instance.IsGameOver==false)
+        {
+            if (GameCoreController.Instance.CurrentTurn != GameCoreController.GameTurnState.OPPONENT)
+            {
+                if (name == "PlayerCharacter")
+                {
+                    charAnim.Play("Armature_ThinkingAction");
+                }
+            }
+            else if (GameCoreController.Instance.CurrentTurn == GameCoreController.GameTurnState.OPPONENT)
+            {
+                if (name == "PlayerCharacter")
+                {
+                    charAnim.Play("Armature_ArmatureAction");
+                }
+            }
+        }
+    }
+
+    public void GameOver()
+    {
+
+        if (GameCoreController.Instance.CurrentTurn == GameCoreController.GameTurnState.PLAYERWON)
+        {
+            if (name == "PlayerCharacter")
+            {
+                charAnim.Play("Armature_WinAction");
+            }
+            else
+            {
+                charAnim.Play("Armature_LoseAction");
+            }
+        }
+        else if (GameCoreController.Instance.CurrentTurn == GameCoreController.GameTurnState.OPPONENTWON)
+        {
+            if (name != "PlayerCharacter")
+            {
+                charAnim.Play("Armature_WinAction");
+            }
+            else
+            {
+                charAnim.Play("Armature_LoseAction");
+            }
+        }
+        else if (GameCoreController.Instance.CurrentTurn == GameCoreController.GameTurnState.GAMETIED)
+        {
+            //At tie, both play lose action
+            charAnim.Play("Armature_LoseAction");
+        }
+    }
+}
